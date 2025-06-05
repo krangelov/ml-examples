0f602b8d048020f058b6d964bb00006eb5b3d968
Krasimir Angelov <kr.angelov@gmail.com>
2025-06-05 04:41:13 +0000

    add domains and verb frames. filter out disconnected words

--- embedding/test.py
+++ embedding/test.py
@@ -10,7 +10,10 @@ from sklearn.neural_network import MLPClassifier
 graph = nx.Graph()
 lex_funs = {}
 lex_cats = {}
-for synset in tqdm(wordnet.all_synsets(), desc="Building graph"):
+synsets = wordnet.all_synsets()
+for i in tqdm(range(len(synsets)), desc="Building the graph"):
+    synset = synsets.pop()
+    countS = 0
     for rel in (synset.hypernyms()+
                 synset.instance_hypernyms()+
                 synset.hyponyms()+
@@ -22,16 +25,39 @@ for synset in tqdm(wordnet.all_synsets(), desc="Building graph"):
                 synset.substance_meronyms()+
                 synset.part_meronyms()):
         graph.add_edge(synset.synsetOffset, rel.synsetOffset)
+        countS += 1
     for lexeme in synset.lexemes():
-        cat = wordnet.w.__pgf__.functionType(lexeme.lex_fun).cat
-        index = lex_cats.setdefault(cat,len(lex_cats))
-        lex_funs[lexeme.lex_fun] = (synset.gloss, index)
-        graph.add_edge(synset.synsetOffset, lexeme.lex_fun)
+        count = countS
         for rel in (lexeme.antonyms()+
                     lexeme.participle()+
                     lexeme.alsosee()+
                     lexeme.derived()):
             graph.add_edge(lexeme.lex_fun, rel.lex_fun)
+            count += 1
+        for domain_id in lexeme.domain_ids:
+            graph.add_edge(lexeme.lex_fun, "D"+str(domain_id))
+            graph.add_edge("D"+str(domain_id), lexeme.lex_fun)
+            count += 1
+        for frame_id in lexeme.frame_ids:
+            graph.add_edge(lexeme.lex_fun, "F"+str(frame_id))
+            graph.add_edge("F"+str(frame_id), lexeme.lex_fun)
+            count += 1
+        if count:
+            graph.add_edge(synset.synsetOffset, lexeme.lex_fun)
+            cat = wordnet.w.__pgf__.functionType(lexeme.lex_fun).cat
+            index = lex_cats.setdefault(cat,len(lex_cats))
+            lex_funs[lexeme.lex_fun] = (synset.gloss, index, cat)
+for domain in wordnet.all_domains():
+    if domain.parent:
+        graph.add_edge("D"+str(domain.id), "D"+str(domain.parent))
+        graph.add_edge("D"+str(domain.parent), "D"+str(domain.id))
+for frame in wordnet.verb_frames():
+    graph.add_edge("F"+str(frame.id), "C"+str(frame.class_id))
+    graph.add_edge("C"+str(frame.class_id), "F"+str(frame.id))
+for cls in wordnet.verb_classes():
+    if cls.super_id:
+        graph.add_edge("C"+str(cls.id), "C"+str(cls.super_id))
+        graph.add_edge("C"+str(cls.super_id), "C"+str(cls.id))
 
 node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=8)
 model = node2vec.fit(window=10, min_count=1, batch_words=4)
@@ -39,15 +65,15 @@ model.save("word2vec.model")
 
     
 with open("vectors.tsv","w") as vf, open("meta.tsv","w") as mf:
-    mf.write("function\tgloss\n")
-    for lex_fun, (gloss, index) in lex_funs.items():
+    mf.write("function\tgloss\tcategory\n")
+    for lex_fun, (gloss, index, cat) in lex_funs.items():
         vf.write("\t".join(map(str,model.wv[lex_fun]))+"\n")
-        mf.write(lex_fun+"\t"+gloss+"\n")
+        mf.write(lex_fun+"\t"+gloss+"\t"+cat+"\n")
 
 
 X = model.wv[lex_funs]
 Y = np.zeros((len(lex_funs), len(lex_cats)))
-for i,(lex_fun,(gloss,index)) in enumerate(lex_funs.items()):
+for i,(lex_fun,(gloss,index,cat)) in enumerate(lex_funs.items()):
     Y[i, index] = 1
 
 clf = MLPClassifier(hidden_layer_sizes=(64), max_iter=800, random_state=1)
@@ -56,5 +82,5 @@ with open('types.model','wb') as f:
     pickle.dump(clf,f)
 
 matching = sum(clf.predict([model.wv[lex_fun]])[0,index]
-                   for lex_fun, (gloss,index) in lex_funs.items())
+                   for lex_fun, (gloss,index,cat) in lex_funs.items())
 print(f"{matching}/{len(lex_funs)}={matching/len(lex_funs)}")
